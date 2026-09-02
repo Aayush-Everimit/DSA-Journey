4b8ac8331f51787067f321a5cc1a80911cde8f23
Aayush Gautam <aayush_2006@outlook.com>
2026-09-03 00:37:41 +0530

    0014-LONGEST-COMMON-PREFIX Stats: Time: 0 ms (100%), Space: 43.4 MB (16.7%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,20 +1,23 @@
-class Solution 
-{
-    public String longestCommonPrefix(String[] strs) 
-    {
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        
+        
         String str = strs[0];
-
-        for(int i=0;i< strs.length; i++)
-        {
-            while(strs[i].indexOf(str)!=0)
-            {
-                str = str.substring(0, str.length()-1);
-                if(str.isEmpty())
-                {
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].length() < str.length()) { 
+                str = strs[i]; 
+            }
+        }
+        
+        for (int i = 0; i < strs.length; i++) {
+            while (strs[i].indexOf(str) != 0) {
+                str = str.substring(0, str.length() - 1);
+                if (str.isEmpty()) {
                     return "";
                 }
-            }    
+            }
         }
         return str;
-    }
+    }      
 }
