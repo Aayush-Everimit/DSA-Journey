500f7a758d568fc4631aac3d782356604083d3d4
Aayush Gautam <aayush_2006@outlook.com>
2026-09-16 20:18:39 +0530

    Time: 0 ms (100%), Space: 46.9 MB (24%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,21 +1,14 @@
-class Solution 
-{
-    public int removeDuplicates(int[] nums) 
-    {
-        LinkedHashSet<Integer> set = new LinkedHashSet<>();
-        for(int num : nums){
-            set.add(num);
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        
+        int uniqueIndex = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[uniqueIndex]) {
+                uniqueIndex++;
+                nums[uniqueIndex] = nums[i];
+            }
         }
-        int index = 0;
-
-        for(int num : set){
-            nums[index++] = num;
-        }
-        while(index < nums.length){
-            nums[index++] = 0;
-        }
-
-
-        return set.size();
+        return uniqueIndex + 1;
     }
 }
